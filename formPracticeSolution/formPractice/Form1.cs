43831a56131c57e98e1181d89fd435dbd3a285d1
David Palacios <dpalacios03081991@gmail.com>
2018-01-26 00:08:10 -0700

    created a form with a message

--- formPracticeSolution/formPractice/Form1.cs
+++ formPracticeSolution/formPractice/Form1.cs
@@ -16,5 +16,10 @@ namespace formPractice
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a form", "Some Form");
+        }
     }
 }
